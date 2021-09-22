a0efb08016aee343d937633959190aa590d7eb03
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2021-09-22 23:09:59 +0200

    trying another way

--- src/components/CardBoard/CardBoard.js
+++ src/components/CardBoard/CardBoard.js
@@ -2,20 +2,38 @@ import React, { useState } from "react";
 import Card from "../Card/Card";
 import classes from "./CardBoard.module.css";
 
-import { initialCards } from "./Photos";
+import Rocky from "../../photos/Rocky.png";
+import Chase from "../../photos/Chase.png";
+import Marshall from "../../photos/Marshall.png";
+import Rubble from "../../photos/Rubble.png";
+import Skye from "../../photos/Skye.png";
+import Zuma from "../../photos/Zuma.png";
+
+const initialCards = [
+  { id: 1, photo: Rocky, flipped: true },
+  { id: 2, photo: Chase, flipped: false },
+  { id: 3, photo: Marshall, flipped: false },
+  { id: 4, photo: Rubble, flipped: false },
+  { id: 5, photo: Skye, flipped: false },
+  { id: 6, photo: Zuma, flipped: false },
+  { id: 7, photo: Rocky, flipped: false },
+  { id: 8, photo: Chase, flipped: false },
+  { id: 9, photo: Marshall, flipped: true },
+  { id: 10, photo: Rubble, flipped: false },
+  { id: 11, photo: Skye, flipped: false },
+  { id: 12, photo: Zuma, flipped: false },
+];
 
 const CardBoard = () => {
-  const [cards, setCards] = useState(initialCards);
+  const [cards, setCards] = useState(
+    initialCards.sort(() => Math.random() - 0.4)
+  );
   const [pairCards, setPairCards] = useState();
 
   const flippedCard = (clickedCard) => {
     const idx = clickedCard.id;
     const findedCard = cards[idx];
-
-    setPairCards([...pairCards, ...findedCard]);
   };
-
-  console.log(pairCards);
 
   const singleCard = cards.map((card, idx) => {
     return (
