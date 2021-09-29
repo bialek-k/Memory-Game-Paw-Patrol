c26eae41a0f91f6559200fe8bcc91a5096e6fbfa
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2021-09-29 23:28:28 +0200

    work almost

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import { useState } from "react";
 import "./App.css";
 import CardBoard from "./components/CardBoard/CardBoard";
+import Card from "./components/Card/Card";
+
+import Rocky from "./photos/Rocky.png";
+import Chase from "./photos/Chase.png";
+import Marshall from "./photos/Marshall.png";
+import Rubble from "./photos/Rubble.png";
+import Skye from "./photos/Skye.png";
+import Zuma from "./photos/Zuma.png";
+
+const initialCards = [
+  { id: 0, photo: Rocky, name: "Rocky", flipped: false },
+  { id: 1, photo: Chase, name: "Chase", flipped: false },
+  { id: 2, photo: Marshall, name: "Marshall", flipped: false },
+  { id: 3, photo: Rubble, name: "Rubble", flipped: false },
+  { id: 4, photo: Skye, name: "Skye", flipped: false },
+  { id: 5, photo: Zuma, name: "Zuma", flipped: false },
+  { id: 6, photo: Rocky, name: "Rocky", flipped: false },
+  { id: 7, photo: Chase, name: "Chase", flipped: false },
+  { id: 8, photo: Marshall, name: "Marshall", flipped: false },
+  { id: 9, photo: Rubble, name: "Rubble", flipped: false },
+  { id: 10, photo: Skye, name: "Skye", flipped: false },
+  { id: 11, photo: Zuma, name: "Zuma", flipped: false },
+];
 
 const App = () => {
+  const [cards, setCards] = useState(initialCards);
+  const [clickedCard, setClickedCard] = useState([]);
+
+  const cardsOnTable = cards.map((card, idx) => {
+    return (
+      <Card
+        id={idx}
+        key={idx}
+        cards={cards}
+        name={card.name}
+        setCards={setCards}
+        photo={card.photo}
+        flipped={card.flipped}
+        clickedCard={clickedCard}
+        setClickedCard={setClickedCard}
+      />
+    );
+  });
+
   return (
     <div className="App">
-      <CardBoard />
+      <CardBoard>{cardsOnTable}</CardBoard>
     </div>
   );
 };
--- src/components/CardBoard/CardBoard.js
+++ src/components/CardBoard/CardBoard.js
@@ -1,60 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import classes from "./CardBoard.module.css";
 
-import Rocky from "../../photos/Rocky.png";
-import Chase from "../../photos/Chase.png";
-import Marshall from "../../photos/Marshall.png";
-import Rubble from "../../photos/Rubble.png";
-import Skye from "../../photos/Skye.png";
-import Zuma from "../../photos/Zuma.png";
-
-const initialCards = [
-  { id: 0, photo: Rocky, name: "Rocky", flipped: false },
-  { id: 1, photo: Chase, name: "Chase", flipped: false },
-  { id: 2, photo: Marshall, name: "Marshall", flipped: false },
-  { id: 3, photo: Rubble, name: "Rubble", flipped: false },
-  { id: 4, photo: Skye, name: "Skye", flipped: false },
-  { id: 5, photo: Zuma, name: "Zuma", flipped: false },
-  { id: 6, photo: Rocky, name: "Rocky", flipped: false },
-  { id: 7, photo: Chase, name: "Chase", flipped: false },
-  { id: 8, photo: Marshall, name: "Marshall", flipped: false },
-  { id: 9, photo: Rubble, name: "Rubble", flipped: false },
-  { id: 10, photo: Skye, name: "Skye", flipped: false },
-  { id: 11, photo: Zuma, name: "Zuma", flipped: false },
-];
-
-const CardBoard = () => {
-  const [cards, setCards] = useState(initialCards);
-  const [clickedCard, setClickedCard] = useState([]);
-
-  if (clickedCard.length >= 2) {
-    if (clickedCard[0].name === clickedCard[1].name) {
-      console.log("ta karta");
-    } else {
-      console.log("szukaj dalej");
-    }
-  }
-
-  // check for end game
-
-  const cardsOnGame = cards.map((card, idx) => {
-    return (
-      <Card
-        id={idx}
-        key={idx}
-        cards={cards}
-        name={card.name}
-        setCards={setCards}
-        photo={card.photo}
-        flipped={card.flipped}
-        clickedCard={clickedCard}
-        setClickedCard={setClickedCard}
-      />
-    );
-  });
-
-  return <div className={classes.CardBoard}>{cardsOnGame}</div>;
+const CardBoard = (props) => {
+  return <div className={classes.CardBoard}>{props.children}</div>;
 };
 
 export default CardBoard;
