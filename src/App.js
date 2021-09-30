ea215494438bf0720dbd29fdff70edf059fb3fbb
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2021-09-30 21:43:05 +0200

    almost done with logic

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardBoard from "./components/CardBoard/CardBoard";
 import Card from "./components/Card/Card";
 
-import Rocky from "./photos/Rocky.png";
-import Chase from "./photos/Chase.png";
-import Marshall from "./photos/Marshall.png";
-import Rubble from "./photos/Rubble.png";
-import Skye from "./photos/Skye.png";
-import Zuma from "./photos/Zuma.png";
-
-const initialCards = [
-  { id: 0, photo: Rocky, name: "Rocky", flipped: false },
-  { id: 1, photo: Chase, name: "Chase", flipped: false },
-  { id: 2, photo: Marshall, name: "Marshall", flipped: false },
-  { id: 3, photo: Rubble, name: "Rubble", flipped: false },
-  { id: 4, photo: Skye, name: "Skye", flipped: false },
-  { id: 5, photo: Zuma, name: "Zuma", flipped: false },
-  { id: 6, photo: Rocky, name: "Rocky", flipped: false },
-  { id: 7, photo: Chase, name: "Chase", flipped: false },
-  { id: 8, photo: Marshall, name: "Marshall", flipped: false },
-  { id: 9, photo: Rubble, name: "Rubble", flipped: false },
-  { id: 10, photo: Skye, name: "Skye", flipped: false },
-  { id: 11, photo: Zuma, name: "Zuma", flipped: false },
-];
+import { initialCards } from "./components/Card/Photos";
 
 const App = () => {
   const [cards, setCards] = useState(initialCards);
   const [clickedCard, setClickedCard] = useState([]);
+  const [finalCards, setFinalCards] = useState(initialCards);
+  const [cardFound, setCardFound] = useState([]);
+
+  // finding pairs of cards
+  useEffect(() => {
+    if (clickedCard.length > 1) {
+      if (
+        clickedCard[0].id !== clickedCard[1].id &&
+        clickedCard[0].name === clickedCard[1].name
+      ) {
+        const cardToFinal = clickedCard[1];
+        setCardFound([cardToFinal]);
+        setClickedCard([]);
+        setFinalCards([...cards]);
+      } else {
+        setClickedCard([]);
+        setTimeout(() => {
+          setCards([...finalCards]);
+        }, 500);
+      }
+    }
+  });
+
+  useEffect(())
 
   const cardsOnTable = cards.map((card, idx) => {
     return (
---
+++ src/components/Card/Photos.js
@@ -0,0 +1,21 @@
+import Rocky from "../../photos/Rocky.png";
+import Chase from "../../photos/Chase.png";
+import Marshall from "../../photos/Marshall.png";
+import Rubble from "../../photos/Rubble.png";
+import Skye from "../../photos/Skye.png";
+import Zuma from "../../photos/Zuma.png";
+
+export const initialCards = [
+  { id: 0, photo: Rocky, name: "Rocky", flipped: false },
+  { id: 1, photo: Chase, name: "Chase", flipped: false },
+  { id: 2, photo: Marshall, name: "Marshall", flipped: false },
+  { id: 3, photo: Rubble, name: "Rubble", flipped: false },
+  { id: 4, photo: Skye, name: "Skye", flipped: false },
+  { id: 5, photo: Zuma, name: "Zuma", flipped: false },
+  { id: 6, photo: Rocky, name: "Rocky", flipped: false },
+  { id: 7, photo: Chase, name: "Chase", flipped: false },
+  { id: 8, photo: Marshall, name: "Marshall", flipped: false },
+  { id: 9, photo: Rubble, name: "Rubble", flipped: false },
+  { id: 10, photo: Skye, name: "Skye", flipped: false },
+  { id: 11, photo: Zuma, name: "Zuma", flipped: false },
+];
